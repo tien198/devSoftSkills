5dd0eab4979b943f5f26deb5e6984a7691e369c8
tien <tienvn998@gmail.com>
2026-01-08 21:54:54 +0700

    update

--- src/pages/uml/Layout.tsx
+++ src/pages/uml/Layout.tsx
@@ -4,6 +4,9 @@ import Header from "../../components/Header";
 const navLinks = [
   "/uml/1.uml-introduce.json",
   "/uml/2.usecase-diagram-vs-story-mapping.json",
+  "/uml/3.class-diagram.json",
+  "/uml/4.sequence-activity-diagram.json",
+  "/uml/5.state-chart-diagram.json",
 ];
 
 export default function UMLLayout() {
