b245d47d55b933c19ffce133f2e77fba8c3647b5
tien998 <tienvn998@gmail.com>
2026-04-01 11:20:13 +0700

    update

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,6 +1,6 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { ArticleData } from "../types/article";
-import { ArticleContext } from "../store/context";
+// import { ArticleContext } from "../store/context";
 import ArticleRenderer from "../components/ArticleRender";
 
 async function getArticle(id: string) {
@@ -10,14 +10,14 @@ async function getArticle(id: string) {
 
 export default function App() {
   const [articleData, setAricleData] = useState<ArticleData | null>(null);
-  const articleContext = use(ArticleContext!);
+  // const articleContext = use(ArticleContext!);
   // const [id, setId] = useState(location.pathname.split("/")[2] ?? "1.readme.json");
-  const { articleId } = articleContext!;
+  // const { articleId } = articleContext!;
 
   useEffect(() => {
-    const id = articleId ?? location.pathname.split("/")[2] ?? "1.readme.json";
+    const id = location.pathname.split("/")[2] ?? "1.readme.json";
     getArticle(id).then((article) => setAricleData(article));
-  }, [articleId]);
+  });
 
   if (!articleData) {
     return <div>Loading ...</div>;
